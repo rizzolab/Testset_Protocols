6eb2cb406fee73d0242aab6727c89ca5eba9c650
Christopher Corbo <chris.corbo.xx@gmail.com>
2024-02-09 18:37:22 -0500

    Additional check for missing cofactor

--- run.001b.check.sh
+++ run.001b.check.sh
@@ -11,6 +11,13 @@ then
    echo ${system}>> zyy.01.lig_missing.dat
 fi
 
+if ls -l zzz.master | grep "$system.cof.moe.mol2";then
 
+   if ! ls -l ${system}/001.lig-prep | grep -q "${system}.cof.am1bcc.mol2"
+   then
+      echo ${system}>> zyy.01.cof_missing.dat
+   fi
+
+fi 
 done
 
